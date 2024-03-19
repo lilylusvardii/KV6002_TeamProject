69c9af49b1b6513b0cb7d4eda0774e78fd4fee26
lilylusvardii <124780940+lilylusvardii@users.noreply.github.com>
2024-03-19 15:47:25 +0000

    updates

--- Login.php
+++ Login.php
@@ -1,23 +1,67 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0"><?php
+		
+ini_set("session.save_path", "/home/unn_w21010751/sessionData");
+session_start();		
+		
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Logging in</title>
 </head>
 <body>
-
-<h2>Log in here</h2>
-
-<p>Use your log in details to access your account</p>
-
-<form action="/action_page.php">
-  <label for="username">Username:</label>
-  <input type="text" id="username" name="username"> 
-  <label for="pwd">Password:</label>
-  <input type="password" id="pwd" name="pwd"><br><br>
-  <input type="submit" value="Submit">
-</form>
-
-</body>
+<main>
+		<?php
+	
+		$username = filter_has_var(INPUT_POST, 'username') ? $_POST['username']: null;
+		$username = trim($username);
+		$password = filter_has_var(INPUT_POST, 'password') ? $_POST['password']: null;
+		$password = trim($password);
+		
+		if (empty($username) || empty($password)) { 
+			echo "<p>please input a username and password. <a href = 'Login.html'>Try here</a></p>\n";
+		}
+		else {
+			try { 
+			
+				unset($_SESSION['username']);
+				unset($_SESSION['logged-in']);
+				
+				require_once("databaseConn.php");
+				$dbConn = getConnection();
+				
+				$sql = "SELECT passwordHash 
+				FROM nmc_users 
+				WHERE username = :username";
+				
+				$stmt = $dbConn->prepare($sql);
+				$stmt->execute(array(':username' => $username));
+				
+				$user = $stmt->fetchObject();
+				if ($user) {
+						if (password_verify($password, $user->passwordHash)) {
+							echo "<p>logged in successfully!</p>";
+							echo "<a href = 'listedRecords.php'>you can edit records here</a>\n";
+							$_SESSION['logged-in'] = true; 
+							$_SESSION['username'] = $username;
+						}
+						else { 
+							echo "<p>the username or password is incorrect please <a href='logOnForm.html'>try again</a></p>\n";
+						}
+						
+				
+				}
+				elseif($user != $username) {
+					echo "<p>the username or password is incorrect please <a href='logOnForm.html'>try again</a></p>\n";
+				}
+						
+			}catch (Exception $e) { 
+				echo "record not found: " . $e->getMessage();}
+		}
+		?>
+		</main>
+	</body>
 </html>
