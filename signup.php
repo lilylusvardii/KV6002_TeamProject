20b5e6169e262e0234b3be392800bc6c7c3786b1
ScorpSab <78867244+ScorpSab@users.noreply.github.com>
2024-03-21 18:14:20 +0000

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+    require 'Database.php' ;
+	
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $cat_id = 2;
+    $location = $_POST['location'];
+	$phonenumber = $_POST['phonenumber'];
+	$icg_id = $_POST['icg_id'];
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password, cat_id, location, phonenumber, icg_id) 
+					VALUES (:username, :password, :cat_id, :location, :phonenumber, :icg_id)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+    $stmt->bindValue(':password', $hashedPassword, SQLITE3_TEXT);
+    $stmt->bindValue(':cat_id', $cat_id, SQLITE3_TEXT);
+	$stmt->bindValue(':location', $location, SQLITE3_TEXT);
+    $stmt->bindValue(':phonenumber', $phonenumber, SQLITE3_TEXT);
+    $stmt->bindValue(':icg_id', $icg_id, SQLITE3_TEXT);
+    $result = $stmt->execute();
+
+    if ($result) {
+        echo "User signed up successfully";
+    } else {
+        echo "Error signing up: " . $conn->lastErrorMsg();
+    }
+
+    $stmt->close();
+} else {
+    echo "Invalid request";
+}
+
+?>
