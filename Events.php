4b55983516d6f7dddc203e65e0387ab7d348e824
lilylusvardii <124780940+lilylusvardii@users.noreply.github.com>
2024-03-22 16:26:51 +0000

    Update Events.php

--- Events.php
+++ Events.php
@@ -3,12 +3,12 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); //php error handing - had an error with the syntax in this document (now functioning) 
 
 //db connection
 require 'Database.php';
 
-$dbConnection = getConnection();
+$dbConnection = getConnection(); //db connection
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['eventName']) && isset($_POST['desc']) && isset($_POST['location']) && isset($_POST['capacity']) && isset($_POST['incomeGroup']) && isset($_POST['date'])) {
@@ -17,24 +17,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $sql = "INSERT INTO em_events (eventname, description, location, capacity, icg_id, date) VALUES (:eventname, :description, :location, :capacity, :icg_id, :date)";
             $stmt = $dbConnection->prepare($sql);
 
-            $stmt->bindParam(':eventname', $_POST['eventName']);
+            $stmt->bindParam(':eventname', $_POST['eventName']); //binding parameters
             $stmt->bindParam(':description', $_POST['desc']);
             $stmt->bindParam(':location', $_POST['location']);
             $stmt->bindParam(':capacity', $_POST['capacity']);
             $stmt->bindParam(':icg_id', $_POST['incomeGroup']);
             $stmt->bindParam(':date', $_POST['date']);
 
-            if ($stmt->execute()) {
-                echo "your event has been added successfully!";
+            //executing the prepared statement 
+            if ($stmt->execute()) { 
+                echo "your event has been added successfully!"; //letting user know event has been added 
                 echo '<a href="index.php"> click here to go back to the home page</a><br>';
                 echo '<a href="Events.html"> click here to go back to the events managment page</a>';
             } else {
-                echo "error, event couldn't be added";
+                echo "error, event couldn't be added"; //error handling
                 echo '<a href="index.php"> click here to go back to the home page</a><br>'
                 echo '<a href="Events.html"> click here to go back to the events managment page</a>';
             }
         } catch (PDOException $e) {
-            echo "database error: " . $e->getMessage();
+            echo "database error: " . $e->getMessage(); //error handling
         }
     }
 } else {
