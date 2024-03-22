3444af2287de54c665e4e8bb5124dfe854b78d6e
lilylusvardii <124780940+lilylusvardii@users.noreply.github.com>
2024-03-22 15:53:00 +0000

    Update Events.php

--- Events.php
+++ Events.php
@@ -1,20 +1,20 @@
 <?php
-//part of adding events subsystem
+//part of adding events subsystem, this is to add new events to database
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+//db connection
 require 'Database.php';
 
 $dbConnection = getConnection();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['eventName']) && isset($_POST['desc']) && isset($_POST['location']) && isset($_POST['capacity']) && isset($_POST['incomeGroup'])) {
+    if (isset($_POST['eventName']) && isset($_POST['desc']) && isset($_POST['location']) && isset($_POST['capacity']) && isset($_POST['incomeGroup'])isset($_POST['date'])) {
         try {
             //preparing sql statements
-            $sql = "INSERT INTO em_events (eventname, description, location, capacity, icg_id) VALUES (:eventname, :description, :location, :capacity, :icg_id)";
+            $sql = "INSERT INTO em_events (eventname, description, location, capacity, icg_id, date) VALUES (:eventname, :description, :location, :capacity, :icg_id, :date)";
             $stmt = $dbConnection->prepare($sql);
 
             $stmt->bindParam(':eventname', $_POST['eventName']);
@@ -22,6 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->bindParam(':location', $_POST['location']);
             $stmt->bindParam(':capacity', $_POST['capacity']);
             $stmt->bindParam(':icg_id', $_POST['incomeGroup']);
+            $stmt->bindParam(':date', $_POST['date']);
 
             if ($stmt->execute()) {
                 echo "your event has been added successfully!";
