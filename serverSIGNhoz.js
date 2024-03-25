3b90e9ff6fbac9db92832f01b31624c46f425844
HozzaLA <162722035+HozzaLA@users.noreply.github.com>
2024-03-25 16:00:52 +0000

    Add files via upload

--- serverSIGNhoz.js
+++ serverSIGNhoz.js
@@ -9,17 +9,17 @@ app.use(bodyParser.json());
 
 
 
-const twilioClient = new twilio('AC3ac89a780083b70ff78362038796e9d7', 'edd8da305f965427994391ba3b001537');
+const twilioClient = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 
 app.post('/signupForm', (req, res) => {
     const phoneNumber = req.body.phone;
-    const verificationCode = Math.floor(100000 + Math.random() * 900000); // Generate a 6-digit random number
+    const verificationCode = Math.floor(100000 + Math.random() * 900000);
 
     twilioClient.messages.create({
         body: `Your verification code is: ${verificationCode}`,
         to: phoneNumber, // Text this number
-        from: '+447449615948' // From a valid Twilio number
+        from: process.env.TWILIO_PHONE_NUMBER // From a valid Twilio number
     })
     .then(message => {
         console.log(message.sid);
