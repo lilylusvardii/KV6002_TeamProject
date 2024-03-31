3461ffa0d3acc2c7d40153bd3b97bfe1cd97b234
lilylusvardii <124780940+lilylusvardii@users.noreply.github.com>
2024-03-31 16:51:38 +0000

    updates

--- indexhoz.php
+++ indexhoz.php
@@ -11,7 +11,7 @@
     //part of adding events subsystem
         session_start();
         //checking if user is admin
-        $isAdmin = isset($_SESSION['isAdmin']) ? $_SESSION['isAdmin'] : false;
+        $isAdmin = isset($_SESSION['isAdmin']) ? $_SESSION['isAdmin'] : true;
     ?>
     <section>
         <h4>Language selection</h4>
